6996958491a9577498a5156f2bc86ff05b10ad3b
buezrico <ricobuez@gmail.com>
2022-06-16 01:52:24 +0100

    fixing font

--- pages/contact.js
+++ pages/contact.js
@@ -12,6 +12,13 @@ const Contact = () => {
           content="Get in touch with Awelga Female Connect"
         />
         <link rel="icon" href="/logo.ico" />
+
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=optional"
+          rel="stylesheet"
+        />
       </Head>
 
       <PagesTop
